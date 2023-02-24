322e2f4736cbc0ab8d9d9da702a54ff142e32b90
Emil F. Lanza <2019120200@gmail.com>
2023-02-24 13:57:29 -0600

    Question 2

--- q2/main.go
+++ q2/main.go
@@ -1,1 +1,18 @@
 package main
+
+import("fmt"
+"time")
+
+func goroutine(){
+	fmt.Println("Goroutine begins")
+	time.Sleep(1*time.Second)
+	fmt.Println("Goroutine finish")
+}
+
+func main(){
+	fmt.Println("Main executes")
+	go goroutine()
+	fmt.Println("Main routine")
+	time.Sleep(3 * time.Second)
+	fmt.Println("Done")
+}
